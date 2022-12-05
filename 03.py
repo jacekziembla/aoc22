3d9c777977b8b3f8195d1bcea358fc4a55bdba19
jacekz <redacted@redacted.redacted>
2022-12-05 23:53:05 +0100

    Task #03

---
+++ 03.py
@@ -0,0 +1,25 @@
+from collections import Counter
+
+with open("03.txt") as f:
+    lines = f.read().split()
+
+letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+values = {char: index + 1 for index, char in enumerate(letters)}
+
+duplicates = []
+for line in lines:
+    duplicates.append((set(line[:len(line) // 2]) & set(line[len(line) // 2:])).pop())
+
+results = [values[letter] for letter in duplicates]
+
+# Part A
+print(sum(results))
+
+# Part B
+duplicates = []
+for index in range(int(len(lines) / 3)):
+    duplicate = set(lines[3 * index]) & set(lines[3 * index + 1]) & set(lines[3 * index + 2])
+    duplicates.append(duplicate.pop())
+
+results = [values[letter] for letter in duplicates]
+print(sum(results))
