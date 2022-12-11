812e92a6877dab75d85301319faae8b22f02e440
jacekz <redacted@redacted.redacted>
2022-12-11 11:55:07 +0100

    Task #11

---
+++ 11.py
@@ -0,0 +1,62 @@
+from __future__ import annotations
+from typing import List
+
+with open("input/11.txt") as f:
+    lines = f.read().replace("* old", "^ 2").split("\n")
+
+
+class Monkey:
+
+    def __init__(self, items: List[int], operator: str, value: int, test_value: int, if_true: int, if_false: int):
+        self.items = items
+        self.operator = operator
+        self.operator_value = value
+        self.test_value = test_value
+        self.if_true = if_true
+        self.if_false = if_false
+        self.counter = 0
+
+    def operation(self, item: int) -> int:
+        if self.operator == "+":
+            new_item = item + self.operator_value
+        elif self.operator == "*":
+            new_item = item * self.operator_value
+        elif self.operator == "^":
+            new_item = item * item
+        else:
+            raise ValueError
+        return int(new_item / 3)
+
+    def test(self, item: int) -> bool:
+        return item % self.test_value == 0
+
+    def inspect_and_throw(self, targets: List[Monkey]):
+        item = self.operation(item=self.items.pop(0))
+        if self.test(item):
+            target = targets[self.if_true]
+        else:
+            target = targets[self.if_false]
+        target.items.append(item)
+        self.counter += 1
+
+
+number_of_monkeys = int(len(lines) / 7)
+monkeys: List[Monkey] = []
+for monkey_index in range(number_of_monkeys):
+    monkeys.append(Monkey(
+        items=[int(item) for item in lines[7 * monkey_index + 1].split(": ")[1].split(", ")],
+        operator=lines[7 * monkey_index + 2].split("old ")[1][0],
+        value=int(lines[7 * monkey_index + 2].split()[-1]),
+        test_value=int(lines[7 * monkey_index + 3].split()[-1]),
+        if_true=int(lines[7 * monkey_index + 4].split()[-1]),
+        if_false=int(lines[7 * monkey_index + 5].split()[-1])
+    ))
+
+for _ in range(20):
+    for monkey in monkeys:
+        for _ in range(len(monkey.items)):
+            monkey.inspect_and_throw(monkeys)
+
+# Part A
+counters = sorted([i.counter for i in monkeys])
+print(counters.pop() * counters.pop())
